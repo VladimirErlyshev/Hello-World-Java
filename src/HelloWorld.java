35f224ef0bad54e97525f3b7d601acd7849dd818
VladimirErlyshev <vladimirerl@gmail.com>
2021-09-13 22:12:20 +0500

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+/**
+ * Author
+ * Body
+ * Head*/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        char bb = '!';
+        System.out.print("Amigo ");
+        System.out.print("The ");
+        System.out.print("Best");
+        System.out.println(bb);
+    }
+}
